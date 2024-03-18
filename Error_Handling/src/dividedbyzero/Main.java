47729aea48e77560d3ce359955fbf01b13d8a4fb
11 <163410183+Francoiszzh@users.noreply.github.com>
2024-03-18 21:15:59 +0800

    try/catch block added

--- Error_Handling/src/dividedbyzero/Main.java
+++ Error_Handling/src/dividedbyzero/Main.java
@@ -6,8 +6,10 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		
+		try {
 		System.out.println(3/0); 
+	}catch(Exception e) {
+		System.out.printf("Caught runtime exception = %s",e.getMessage());
 	}
-	
+	}
 }
